5ebc12e41284eadec553facc8ab71bcc9df44dc9
Maryna <maryna.pylypchenko@gmail.com>
2020-04-28 11:30:30 +0300

    commit

--- JS/lesson3/task1/index.js
+++ JS/lesson3/task1/index.js
@@ -1,6 +1,3 @@
-//const getTotalPrice = arr => {
-//   const sum = arr.reduce((accumulator, currentValue) => accumulator + currentValue)
-//  return '$' + Math.floor(sum * 100) / 100;
 const getTotalPrice = arr => {
     const sum = numberLists.reduce(function callback(acc, num) {
         return acc + num;
