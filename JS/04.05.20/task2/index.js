218802bd54bab175fad37d1886a24434923a0abd
Maryna <maryna.pylypchenko@gmail.com>
2020-05-04 11:45:43 +0300

    commit

--- JS/04.05.20/task2/index.js
+++ JS/04.05.20/task2/index.js
@@ -1,18 +1,14 @@
-//const mergeObjectsV1 = (obj1, obj2) => {
-//  const newObj = Object.assign({}, obj1, obj2);
-//  return newObj;
-
 function mergeObjectsV1(obj1, obj2) {
     return Object.assign({}, obj1, obj2)
 }
 
 
 function mergeObjectsV2(obj1, obj2) {
-    return Object.assign({}, obj2, obj1); ///  мутируется obj2
+    return Object.assign({}, obj2, obj1) ///  мутируется obj2
 }
 
 function mergeObjectsV3(obj1, obj2) {
-    return {...obj1, ...obj2 };
+    return {...obj1, ...obj2 }
 };
 
 
@@ -20,5 +16,5 @@ function mergeObjectsV3(obj1, obj2) {
     return {
         ...obj2,
         ...obj1
-    };
+    }
 };
