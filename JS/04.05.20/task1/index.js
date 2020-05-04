1f4c5ebdf7b779e909be7e13f648497c04fbc431
Maryna <maryna.pylypchenko@gmail.com>
2020-05-04 11:26:36 +0300

    commit

--- JS/04.05.20/task1/index.js
+++ JS/04.05.20/task1/index.js
@@ -1,39 +1,15 @@
-//const addPropertyV1 = (userData, userId) => {
-//   userData.id = userId;
-//return userData;
-//}
-
 //input: object, string
 //returns: object
 function addPropertyV1(userData, userId) {
     userDate.id = userId;
     return userData;
-
 }
-//const addPropertyV2 = (userData, userId) => {
 
-// const newObj = Object.assign(
-// userData, {
-//   id: userId
-//  });
-// return newObj;
-//}
-//function addPropertyV2(userData, userId) {
-//   Object.assign(userData, { id: userId });
-// return userData;
-
-//}
 const addPropertyV2 = (userData, userId) => Object.assign(userData, { id: userId });
 //code readybility 
 const addPropertyV3 = (userData, userId) =>
     Object.assign({}, userData, { id: userId });
 
-
-//const addPropertyV4 = (userData, userId) => {
-//   let copy = {...userData };
-//   copy.id = userId;
-//  return copy;
-//}
 function addPropertyV4(userData, userId) {
     return {...userData, id: userId };
 }
