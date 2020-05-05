5041435cf25a35b8ba0a03f60fd8c6348a6e1db7
Maryna <maryna.pylypchenko@gmail.com>
2020-05-05 12:14:20 +0300

    commit

--- JS/05.05.20/task1/index.js
+++ JS/05.05.20/task1/index.js
@@ -1,22 +1,3 @@
-//const buildObject = (keysList, valuesList) => {
-//   const arr = [];
-
-//   for (let i = 0; i < keysList.length; i++) {
-//        arr.push([keysList[i], valuesList[i]]);
-//  }
-
-//  const obj = Object.fromEntries(arr);
-
-//  return obj;
-//};
-
-//const buildObject = (keysList, valuesList) => {
-//   let obj ={};
-//   for (let i = 0; i < keysList.length; i++){
-//       obj ={...obj,[keyList[i]]: valueList[i] }
-//   }
-//   return obj 
-//}
 function buildObject(keyList, valuesList) {
     keyList.reduce((acc, key, index) => {
         return {
