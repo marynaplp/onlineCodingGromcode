bc947470b49bc991ad82201038902090cc281df4
Maryna <maryna.pylypchenko@gmail.com>
2020-05-05 12:48:25 +0300

    commit

--- JS/05.05.20/task1/index.js
+++ JS/05.05.20/task1/index.js
@@ -1,5 +1,5 @@
-function buildObject(keyList, valuesList) {
-    return keyList.reduce((acc, key, index) => {
+function buildObject(keysList, valuesList) {
+    return keysList.reduce((acc, key, index) => {
         return {...acc,
             [key]: valuesList[index]
         } /// перезаписивает  значение  
