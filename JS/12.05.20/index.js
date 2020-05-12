372bce5726d656f2ec3fc7a09996c273a69a9e69
Maryna <maryna.pylypchenko@gmail.com>
2020-05-12 16:02:46 +0300

    commit

--- JS/12.05.20/index.js
+++ JS/12.05.20/index.js
@@ -2,16 +2,21 @@
 // outcome:function
 
 
-function makeCounter() {
-    let counter = 0;
-    return function count() {
 
-        return ++counter;
+export const makeCounter = () => {
+    let count = 0;
+    return function() {
+        return count++;
     };
 };
-
 const counter1 = makeCounter();
 const counter2 = makeCounter();
+
+console.log(counter1());
+console.log(counter1());
+counter1();
+counter1();
+counter2();
 //  мы постоянно  будет перезаписиватся наше значение
 
 //export { makeCounter }
