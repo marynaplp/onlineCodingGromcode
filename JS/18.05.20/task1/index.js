32a5cb27eb2af1108bcec2dfee2cdc0f28a7dc0b
Maryna <maryna.pylypchenko@gmail.com>
2020-05-18 15:51:25 +0300

    commit

--- JS/18.05.20/task1/index.js
+++ JS/18.05.20/task1/index.js
@@ -5,22 +5,24 @@ export const timer = {
     startTimer() {
         this.timerId = setInterval(() => {
 
-            this.secondsPassed += 5;
+            this.secondsPassed += 1;
             if (this.secondsPassed === 60) {
                 this.minsPassed += 1;
                 this.secondsPassed = 0;
             }
-        }, 5000);
+        }, 1000);
     },
     getTime() {
-        let secs = this.secondsPassed < 10 ? `0${this.secondsPassed}` : this.secondsPassed()
+        let secs = this.secondsPassed < 10 ? `0${this.secondsPassed}` : this.secondsPassed
         return `{this.minsPassed}: ${secs}`;
 
     },
     stopTimer() {
         clearInterval(this.timerId);
+        this.timerId = null;
     },
     resetTimer() {
+        this.stopTimer()
         this.secondsPassed = 0;
         this.minsPassed = 0;
     },
