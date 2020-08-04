dcf204426356b5de4dd27f130bb26380b8270965
Maryna <maryna.pylypchenko@gmail.com>
2020-08-04 13:01:23 +0300

    co

--- JS/04.08.2020/task1/index.js
+++ JS/04.08.2020/task1/index.js
@@ -11,36 +11,25 @@
 
 function filterArray(arr, callback) {
     const result = [];
+
     for (let i = 0; i < arr.length; i++) {
-        if (callback(arrElement, index, entireArr)) {
-            result.push(arrElement);
-        }
+        if (callback(arr[i], i, arr)) result.push(arr[i])
     }
-    return result;
+
+    return result
 }
-//Testing
-// let array = [10, 30, 1, 8, 100, 20, 21, 123];
-
-// const callback = (element) => {
-//     const rest = element > 20;
-//     return rest
-// }
-// filterArray(array, callback)
-//     //console.log(array.filter(callback))
-
-
-
 // //Testing filter
+let array = [10, 30, 1, 8, 122];
+const callback = element => element > 20;
+
+console.log(filterArray(array, callback))
+    //console.log(array.filter(callback))
 
 
-// let array1 = [10, 30, 1, 8, 100];
+
+
 
 // const callback1 = (element, index, array) => {
 //     console.log("ELEMENT: " + element);
 //     console.log("INDEX: " + index);
 //     console.log("ARRAY: " + array);
-
-
-//     const res = element > 20;
-//     return res
-// }
