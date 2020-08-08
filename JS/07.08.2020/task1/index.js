74ab8d4281d0c3277808b033d7b0ffa531f8e436
Maryna <maryna.pylypchenko@gmail.com>
2020-08-08 15:18:37 +0300

    co

--- JS/07.08.2020/task1/index.js
+++ JS/07.08.2020/task1/index.js
@@ -13,12 +13,14 @@
 //2. iterate obj1 key
 //compare values, if not equal - false
 
-function compareObject(obj1, obj2) {
+function compareObjects(obj1, obj2) {
     const keys1 = Object.keys(obj1)
     const keys2 = Object.keys(obj2)
+
     if (keys1.length !== keys2.length) {
         return false
     }
+
     return keys1.reduce((acc, key) =>
         obj1[key] === obj2[key], true
     )
