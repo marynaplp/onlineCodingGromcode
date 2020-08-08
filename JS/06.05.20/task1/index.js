8ae4e5023274596f9a3b5f6502c4b0dca59f9378
Maryna <maryna.pylypchenko@gmail.com>
2020-08-08 15:13:05 +0300

    co

--- JS/06.05.20/task1/index.js
+++ JS/06.05.20/task1/index.js
@@ -1,6 +1,7 @@
 function compareObjects(obj1, obj2) {
     const keys1 = Object.keys(obj1)
     const keys2 = Object.keys(obj2)
+
     if (keys1.length !== keys2.length) {
         return false
     }
