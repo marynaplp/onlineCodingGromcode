8de0db9060468fc17c25eb0f621a8bb204f44d0e
Maryna <maryna.pylypchenko@gmail.com>
2020-08-20 11:36:54 +0300

    commit

---
+++ JS/20.08.2020/index.js
@@ -0,0 +1,68 @@
+const listElem = document.querySelector('.list');
+const tasks = [{
+        text: 'Buy milk',
+        done: false
+    },
+    {
+        text: 'Pick up Tom from airport',
+        done: false
+    },
+    {
+        text: 'Visit party',
+        done: false
+    },
+    {
+        text: 'Visit doctor',
+        done: true
+    },
+    {
+        text: 'Buy meat',
+        done: true
+    },
+];
+const renderTasks = tasksList => {
+    const tasksElems = tasksList
+        .sort((a, b) => a.done - b.done)
+        .map(({
+            text,
+            done
+        }) => {
+            const listItemElem = document.createElement('li');
+            listItemElem.classList.add('list__item');
+            const checkbox = document.createElement('input');
+            checkbox.setAttribute('type', 'checkbox');
+            checkbox.checked = done;
+            checkbox.classList.add('list__item-checkbox');
+            if (done) {
+                listItemElem.classList.add('list__item_done');
+            }
+            listItemElem.append(checkbox, text);
+            debugger;
+            return listItemElem;
+        });
+    listElem.append(...tasksElems);
+};
+renderTasks(tasks);
+
+// How Web works
+//1.add actual data
+//2.render
+//Aldo
+//1.add eventLister
+//2.create task object
+//3.add task to the array
+//4.re render
+const addTask = () => {
+    const inputEl = document.querySelector('.task-input');
+    const countTasks = tasks.length + 1;
+    if (!inputEl.value) return false;
+    const task = {
+        text: inputEl.value,
+        done: false,
+        id: countTasks.toString(),
+    }
+    tasks.push(task)
+    renderTasks(tasks)
+}
+const createBtn = document.querySelector('.create-task-btn')
+createBtn.addEventListener('click', addTask)
