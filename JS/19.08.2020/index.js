ceb93e4d6641e5d9849da772e38e052bfa27d9cf
Maryna <maryna.pylypchenko@gmail.com>
2020-08-25 12:54:17 +0300

    commit

--- JS/19.08.2020/index.js
+++ JS/19.08.2020/index.js
@@ -1,12 +1,12 @@
-// function printMessage(age, city) {
-//     console.log(this);
-//     console.log(`Hello ${this.name}, email ${this.email}, age: ${age}, city: ${city}`);
-// }
-// let user = {
-//     name: "Alex",
-//     email: "gmail.com",
-// }
-// printMessage(100, "Lviv")
+function printMessage(age, city) {
+    console.log(this);
+    console.log(`Hello ${this.name}, email ${this.email}, age: ${age}, city: ${city}`);
+}
+let user = {
+        name: "Alex",
+        email: "gmail.com",
+    }
+    //printMessage(100, "Lviv")
 
 // //input: obj, arg1 ....argN
 // // output: func
@@ -16,4 +16,41 @@
 // printMessageBinded(100, "Lviv")
 
 //input:func, obj, arg1 ...argN
-//input
+//input:
+
+const myBind = (func, context, ...args) => {
+        const myBind = (func, context, ...args) => {
+            console.log("Step1")
+            return function() {
+                console.log("Step2")
+                return func.call(context, ...args)
+            }
+        }
+        const printMessageBinded = myBind(printMessage, user, 200, "Kiev");
+        printMessageBinded()
+
+
+        const myBind1 = (func, context, ...args) => {
+            console.log("Step 1");
+            return function(...targerArgs) {
+                console.log("Step2");
+                return func.call(context, ...args, ...targetArgs);
+            }
+        }
+
+        const printMessageBinded2 = myBind1(printMessage, user, 200);
+        printMessageBinded2("Lviv")
+
+
+        //Option 2
+
+        const mySuperBind = (func, context, ...args) => {
+            console.log(context)
+            context.tempFunction = func;
+            console.log(context);
+            context.tempFunction(...args)
+        }
+
+        constprintMessageBinded3 = mySuperBind(printMessage, user, 50)
+
+        printMessageBinded3();
