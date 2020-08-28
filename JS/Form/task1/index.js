d5f0246f31296fb3599d338df2a2b22f8d0d0882
Maryna <maryna.pylypchenko@gmail.com>
2020-08-28 11:49:59 +0300

    commit

---
+++ JS/Form/task1/index.js
@@ -0,0 +1,45 @@
+const emailInput = document.querySelector('#email');
+const emailErrorText = document.querySelector('.error-text_email');
+const passwordInput = document.querySelector('#password');
+const passwordErrorText = document.querySelector('.error-text_password');
+
+const formElem = document.querySelector('.login-form');
+
+//1.readform data after submit
+//2.validate e-mail
+//3.validate password
+const validateEmail = (email) => {
+    return email.includes("@") ? null : 'Incorrect email'
+}
+const handleEmail = (event) => {
+
+        const email = event.target.value;
+        const error = email ? validateEmail(mail) : "Email required";
+        emailErrorText.textContent = error;
+    }
+    // const handleEmail = (event) => {
+    //     let error = event.target.value ? null : "Email required";
+    //     if (!error) {
+    //         error = event.target.value.includes("@") ? null : 'Incorrect email'
+    //     }
+    //     emailErrorText.textContent = error
+    // }
+
+
+emailInput.addEventListener("input", handleEmail);
+const handlePassword = (event) => {
+    const error = event.target.value ? null : "Password required";
+    passwordErrorText.textContent = error;
+
+
+}
+
+passwordInput.addEventListener('input', handlePassword);
+
+
+const handleSubmit = () => {
+    event.preventDefault();
+    const formData = JSON.stringify(Object.fromEntries(new FormData(formElem)));
+    alert(formData);
+}
+formElem.addEventListener('submit', handleSubmit)
